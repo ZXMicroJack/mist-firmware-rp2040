d3dd9b3cc03c5ec95899420f94c12ed400ec3fee
MicroJack <zx.micro.jack@gmail.com>
2024-04-29 23:29:47 +0100

    V2.5

--- build/version.h
+++ build/version.h
@@ -1,6 +1,6 @@
 #define VERSION_MAJOR	2
-#define VERSION_MINOR	4
-#define VERSION "2.4"
+#define VERSION_MINOR	5
+#define VERSION "2.5"
 #if 0
-setversion 02 04
+setversion 02 05
 #endif
--- rp2u-build/version.h
+++ rp2u-build/version.h
@@ -1,5 +1,5 @@
 #define VERSION_MAJOR	2
-#define VERSION_MINOR	4
+#define VERSION_MINOR	5
 #if 0
-setversion 02 04
+setversion 02 05
 #endif

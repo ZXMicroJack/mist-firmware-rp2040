8b4a86fbf844cfa3881f16ef0a6036d3f7ac5835
MicroJack <zx.micro.jack@gmail.com>
2024-07-25 22:30:14 +0100

    Setting versions.

--- build/version.h
+++ build/version.h
@@ -1,6 +1,6 @@
 #define VERSION_MAJOR	2
-#define VERSION_MINOR	7
-#define VERSION "2.7"
+#define VERSION_MINOR	8
+#define VERSION "2.8"
 #if 0
-setversion 02 07
+setversion 02 08
 #endif
--- rp2u-build/version.h
+++ rp2u-build/version.h
@@ -1,5 +1,5 @@
 #define VERSION_MAJOR	2
-#define VERSION_MINOR	7
+#define VERSION_MINOR	8
 #if 0
-setversion 02 07
+setversion 02 08
 #endif
